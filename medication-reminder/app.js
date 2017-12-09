bdc4a485d1c7cc25751aab483ccb6302f4c28d04
Dawn Mills <dmboot17@gmail.com>
2017-12-09 04:33:15 -0500

    Update app.js

--- medication-reminder/app.js
+++ medication-reminder/app.js
@@ -1,8 +1,9 @@
 'use strict';
 
-require('serve-favicon');
+// require('serve-favicon');
 const express = require('express');
 const path = require('path');
+const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
